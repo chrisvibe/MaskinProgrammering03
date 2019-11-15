fc10484d086c7cb45971852c7b1986593c8497a4
chrisvibe <vibechris@gmail.com>
2019-11-15 14:50:36 +0100

    screen update should workk....

--- OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -5,8 +5,6 @@
 #include <sys/ioctl.h>
 #include <stdio.h>
 #include <stdint.h>
-// #include <sys/stat.h>
-// #include <unistd.h>
 
 void refresh_display(int fbfd, int x, int y, int width, int height);
 
@@ -14,42 +12,5 @@ int main(int argc, char *argv[])
 {
    printf("Hello World, I'm game!\n");
 
-   int d_height = 240;
-   int d_width = 320;
-   int * addr;
-
-   int length = d_height * d_width * 2; // length in bites
-
-   // open the frame buffer for read/write
-   int fbfd = open("/dev/fb0", O_RDWR);
-
-   // get address where we can store pixels (write implies read too)
-   addr = mmap(NULL, length, PROT_WRITE, MAP_SHARED, fbfd, 0);
-
-   int x = 0;
-   int y = 0;
-   for (x = 0; x < d_height; x++) {
-        for (y = 0; y < d_width; y++) {
-            addr[x * d_width + y] = 0xF;
-        }
-   }
-
-   refresh_display(fbfd, 0, 0, d_width, d_height);
-
    exit(EXIT_SUCCESS);
 }
-
-void refresh_display(int fbfd, int x, int y, int width, int height) {
-    // setup which part of the frame buffer that is to be refreshed
-    // for performance reasons, use as small rectangle as possible
-    struct fb_copyarea rect;
-
-    rect.dx = x;
-    rect.dy = y;
-    rect.width = width;
-    rect.height = height;
-
-    // command driver to update display
-    ioctl(fbfd, 0x4680, &rect);
-}
-

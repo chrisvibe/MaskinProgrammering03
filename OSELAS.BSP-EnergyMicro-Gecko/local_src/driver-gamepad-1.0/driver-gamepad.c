de04df837c9da4f77c98da6513552d23a431597f
Ruben S. Sevaldson <r.sevaldson@gmail.com>
2019-11-19 20:17:43 +0100

    Add better loggin

--- OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -30,7 +30,7 @@ static ssize_t my_read (struct  file *filp, char __user *buff, size_t count, lof
   printk("reading\n");
   unsigned int res;
   res = ioread32(gpioMapReturn + 72 + 28);
-  printk("Got result %d\n", res);
+  printk("Got result: %d\n", res);
   return 0;
 }
 
@@ -58,7 +58,7 @@ struct cdev my_cdev = {
 
 
 /*
- * template_init - function to insert this module into kernel space
+ * gamepad_init - function to insert this module into kernel space
  *
  * This is the first of two exported functions to handle inserting this
  * code into a running kernel
@@ -68,6 +68,8 @@ struct cdev my_cdev = {
 
 static int __init gamepad_init(void)
 {
+  printk("Hello from gamepad driver init function. Initializing...\n")
+
   // Need to allocate variables here because C90 restrictions
   int alloc_chrdevice_result;
   int dev_major;
@@ -79,27 +81,25 @@ static int __init gamepad_init(void)
   unsigned int CMU_HFPER;
   unsigned int result;
 
-  printk("Hello World, here is your module speaking\n");
 
-  printk("Allocating for GPIO\n");
+  printk("Allocating memory region for GPIO\n");
   // Request memory. Dont know if 1024 byte is correct, just a guess.
   gpioAlloc = "GPIO";
   if (request_mem_region(GPIO_PA_BASE, 1024, gpioAlloc) == NULL)  {
     printk("An error occured! Could not reserve memory region for GPIO\n");
     return 1;
   }
-  printk("Allocating for CMU\n");
+  printk("Allocating memory region for CMU\n");
   cmuAlloc = "CMU";
   if (request_mem_region(CMU_BASE2, 1024, cmuAlloc) == NULL)  {
     printk("An error occured! Could not reserve memory region for CMU\n");
     return 1;
   }
 
-
   // This is our io address space, but dont read it directlu, use accessor functions
-  printk("io remap for GPIO\n");
+  printk("Initializing io memory remap for GPIO\n");
   gpioMapReturn = ioremap_nocache((resource_size_t) GPIO_PA_BASE, 1024);
-  printk("io remap for CMU\n");
+  printk("Initializing io memory remap for CMU\n");
   cmuMapReturn = ioremap_nocache((resource_size_t) CMU_BASE2, 1024);
   
   printk("Getting device number\n");
@@ -125,39 +125,30 @@ static int __init gamepad_init(void)
   cl = class_create(THIS_MODULE, "Gamepad");
   device_create(cl, NULL, *devno , NULL, "Gamepad");
 
-
-	/* *CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_GPIO;	/1* Enable GPIO clock *1/ */
+  // CMU setup
   // NOTE!!! Here, CMU_HFPERCLKEN0 is a pointer while CMU2_HFPERCLKEN0_GPIO is an int
-  printk("reading from cmu\n");
+  printk("Setting up CMU\n");
   CMU_HFPER = ioread32(cmuMapReturn + 68); 
-  printk("result from cmu_hfper is %d", CMU_HFPER);
-  printk("oring result\n");
   result = CMU_HFPER | CMU2_HFPERCLKEN0_GPIO;
-  /* iowrite32(result, CMU_HFPERCLKEN0); */
-  printk("writing result: %d to cmu\n", result);
+  printk("Writing result: %d to cmu\n", result);
   iowrite32(result, (cmuMapReturn + 68));
 
-	/* Button setup */
-	/* ############################################################ */
-	// Set pins 0-7 to input by writing 0x33333333 to GPIOPCMODEL
-	/* *GPIO_PC_MODEL = 0x33333333; */
+	// Button setup 
   printk("writing to gpio to enable buttons as input\n");
-  // Need to offset gpio_base_c AND gpio_c_din
   iowrite32((unsigned int) 0x33333333, (gpioMapReturn + 72 + 4));
-  printk("writing to gpio to enable buttons as input\n");
 
+  // Enable internal pull-up for buttons by writing 0xff to GPIOPCDOUT
   printk("Setting internal pull up resistors\n");
-  // •Enable internal pull-up for buttons by writing 0xff to GPIOPCDOUT
   iowrite32((unsigned int) 0xff, (gpioMapReturn + 72 + 12));
-  printk("Done setting internal pull up resistors for buttons\n");
 
+  printk("Init function complete, driver should now be visible under /dev\n")
   return 0;
 }
 
 
 static void __exit gamepad_cleanup(void)
 {
-	 printk("Gamepad module running cleanup...\n");
+	 printk("Gamepad driver cleanup\n");
    unregister_chrdev_region(*devno, 1);
    device_destroy(cl, *devno);
    class_destroy(cl);

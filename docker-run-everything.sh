75f6eba0d29c2e42bae1b491892a97bf2e3e5832
Ruben S. Sevaldson <r.sevaldson@gmail.com>
2019-11-19 18:00:52 +0100

    Improve dockerfile

--- docker-run-everything.sh
+++ docker-run-everything.sh
@@ -1,2 +1,2 @@
 docker build -t ex3-image .
-docker run --mount source=myvol4,target=/home/non-root/MaskinProgrammering03 --privileged ex3-image
+docker run --mount source=myvol5,target=/home/non-root/MaskinProgrammering03 --privileged ex3-image

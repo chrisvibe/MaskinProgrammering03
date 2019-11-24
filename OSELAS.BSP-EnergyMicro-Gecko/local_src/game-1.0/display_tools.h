9ee913c268b787a2ba77a1a99ffe39804e4ad322
Ruben S. Sevaldson <r.sevaldson@gmail.com>
2019-11-24 02:51:51 +0100

    Make comments in display_tools.h concistent

--- OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/display_tools.h
+++ OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/display_tools.h
@@ -6,9 +6,9 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define HEIGHT 240 // display dimensions
+#define HEIGHT 240 /* display dimensions */
 #define WIDTH 320
-#define LENGTH HEIGHT *WIDTH * 2 // length in bites
+#define LENGTH HEIGHT *WIDTH * 2 /* length in bites */
 
 struct Settings;
 struct Settings setup_display();
